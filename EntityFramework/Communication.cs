8bf4aece035130783b2279ba196f50b80ed55d90
J Robinson <jrobinson@touro.edu>
2018-03-14 18:00:19 -0400

    Add project files.

---
+++ EntityFramework/Communication.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityFramework
+{
+    public class Communication
+    {
+        public int CommunicationId{ get; set; }
+        public DateTime TimeStamp {get; set; }
+
+        // Navigation Property ..not a real DB field
+        public Sender Sender { get; set; }
+        public List<Receiver> Receivers { get; set; }
+
+    }
+
+    public class Sender
+    {
+        public int SenderId { get; set; }
+
+        [MaxLength(100)]
+        public string Email { get; set; }
+    }
+
+    public class Receiver
+    {
+        public int ReceiverId { get; set; }
+
+        [MaxLength(100)]
+        public string Email { get; set; }
+    }
+}
---
+++ EntityFramework/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    class MySuggestionDb : DbContext
+    {
+        public DbSet<Communication> Communications { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new MySuggestionDb();
+
+            db.Communications.Add(new Communication
+            {
+                TimeStamp = DateTime.UtcNow,
+                Sender = new Sender { Email = "Yaakov"},
+                Receivers = new List<Receiver> {
+                    new Receiver{ Email = "Menashe"},
+                    new Receiver{ Email = "Moshe"}}
+
+            });
+            db.SaveChanges();
+            Console.WriteLine(db.Communications.Count());
+
+            foreach (Communication c in db.Communications)
+            {
+                Console.WriteLine(c.Sender.Email + "=>>>" + c.Receivers.Count);
+            }
+            Console.ReadLine();
+        }
+    }
+}
